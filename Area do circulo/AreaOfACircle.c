6d8c46cf1548ad3f5d4814d70e92f250e220ea39
Sancto <arissonsantos2002@gmail.com>
2022-10-16 22:02:02 -0300

    commit numero 1

---
+++ Area do circulo/AreaOfACircle.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+
+    double R;
+    double A;
+
+    scanf("%lf", &R);
+
+    A=3.14159*(R*R);
+     printf("A=%.4lf\n",A);
+
+return 0;
+}
---
+++ Extremely Basic/ExtremlyBasic-BeeCrowd.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+ 
+int main() { 
+    int A, B;
+    int X;
+
+    scanf("%d", &A);
+    scanf("%d", &B);
+
+    X=A+B;
+
+    printf("X = %d\n", X);
+
+    return 0;
+}
