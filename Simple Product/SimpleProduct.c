d7e83efd6e2fb0493e445bbc02384ab20bd61cd7
Sancto <arissonsantos2002@gmail.com>
2022-10-17 21:52:22 -0300

    Simple product e Simple Sum

---
+++ Simple Product/SimpleProduct.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+
+    int A,B;
+    int prod;
+
+    scanf("%d", &A);
+    scanf("%d", &B);
+    
+    prod = A * B;
+
+    printf("PROD = %d\n", prod);
+
+
+    return 0;
+}
---
+++ Simple Sum/SimpleSum.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+
+    int A,B;
+    int soma;
+
+    scanf("%d", &A);
+    scanf("%d", &B);
+    
+    soma = A + B;
+
+    printf("SOMA = %d\n", soma);
+
+    return 0;
+}
